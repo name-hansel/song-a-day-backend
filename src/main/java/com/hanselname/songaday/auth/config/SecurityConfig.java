8b1147ed5cc73b6a529a24725bbac7e8d9ee7007
Snehal Mane <snehal6mane@gmail.com>
2026-02-15 17:26:51 +0530

    Return 401 if not authorized

--- src/main/java/com/hanselname/songaday/auth/config/SecurityConfig.java
+++ src/main/java/com/hanselname/songaday/auth/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.hanselname.songaday.auth.filter.JWTFilter;
 import com.hanselname.songaday.auth.utils.AuthUtils;
 import com.hanselname.songaday.common.CommonUtils;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,7 +26,9 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers(CommonUtils.AUTH_USER_API_PREFIX + "/**", CommonUtils.SONG_API_PREFIX + "/**").authenticated().anyRequest().permitAll()).oauth2Login(oauth -> oauth.successHandler(successHandler)).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).logout(l -> l.logoutUrl(CommonUtils.API_PREFIX + "/logout").logoutSuccessHandler(((request, response, authentication) -> {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).exceptionHandling(ex -> ex.authenticationEntryPoint((req, res, e) -> {
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        })).authorizeHttpRequests(auth -> auth.requestMatchers("/oauth2/**").permitAll().anyRequest().authenticated()).oauth2Login(oauth -> oauth.successHandler(successHandler)).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).logout(l -> l.logoutUrl(CommonUtils.API_PREFIX + "/logout").logoutSuccessHandler(((request, response, authentication) -> {
             Cookie cookie = new Cookie(AuthUtils.COOKIE_NAME, "");
             cookie.setMaxAge(0);
             cookie.setPath("/");
