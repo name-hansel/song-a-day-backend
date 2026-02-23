fc8b730d238c6501ff9a5afc6d2b3b8eb2aa6397
Snehal Mane <snehal6mane@gmail.com>
2026-02-23 23:33:55 +0530

    Remove formattedDateForToday as it is calculated on frontend now

--- src/main/java/com/hanselname/songaday/user/dto/AuthAppUserResponseDTO.java
+++ src/main/java/com/hanselname/songaday/user/dto/AuthAppUserResponseDTO.java
@@ -3,6 +3,5 @@ package com.hanselname.songaday.user.dto;
 import java.util.UUID;
 
 public record AuthAppUserResponseDTO(UUID uuid, String appUserName,
-                                     String timezone,
-                                     String formattedDateForToday) {
+                                     String timezone) {
 }
--- src/main/java/com/hanselname/songaday/user/mapper/AppUserMapper.java
+++ src/main/java/com/hanselname/songaday/user/mapper/AppUserMapper.java
@@ -1,14 +1,12 @@
 package com.hanselname.songaday.user.mapper;
 
-import com.hanselname.songaday.user.AppUserUtils;
 import com.hanselname.songaday.user.dto.AuthAppUserResponseDTO;
 import com.hanselname.songaday.user.entity.AppUserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = AppUserUtils.class)
+@Mapper(componentModel = "spring")
 public interface AppUserMapper {
     @Mapping(source = "spotifyDisplayName", target = "appUserName")
-    @Mapping(source = ".", target = "formattedDateForToday", qualifiedByName = "getFormattedDateForAppUser")
     AuthAppUserResponseDTO toDTO(AppUserEntity appUserEntity);
 }
