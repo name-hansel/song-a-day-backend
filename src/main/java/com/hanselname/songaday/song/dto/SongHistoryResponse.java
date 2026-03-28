6819d65bb498f0aa089a0ee68aded4ec09187ca1
Snehal Mane <snehal6mane@gmail.com>
2026-03-28 21:53:24 +0530

    Rename field to history

--- src/main/java/com/hanselname/songaday/song/dto/SongHistoryResponse.java
+++ src/main/java/com/hanselname/songaday/song/dto/SongHistoryResponse.java
@@ -3,5 +3,5 @@ package com.hanselname.songaday.song.dto;
 import java.time.LocalDate;
 import java.util.List;
 
-public record SongHistoryResponse(List<SongResponseDTO> songHistory, LocalDate nextDate, boolean hasMore) {
+public record SongHistoryResponse(List<SongResponseDTO> history, LocalDate nextDate, boolean hasMore) {
 }
